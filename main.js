c6aefce379acee792e1bf3a53693a64d0ec81c67
Crimsontyphon7 <lamancio87@gmail.com>
2022-01-30 21:23:50 -0600

    finished this project

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // uses strict mode so strings are not coerced, variables are not hoisted, etc... 
 'use strict';
-
 // brings in the assert module for unit testing
 const assert = require('assert');
 // brings in the readline module to access the command line
@@ -9,15 +8,34 @@ const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
-});
 
+  
+});
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
   // Write code here
   // Use the unit test to see what is expected
+ hand1 = hand1.trim().toLowerCase(); //use dot notation to apply two methods to a variable
+ hand2 = hand2.trim().toLowerCase();
 
-}
+// Using nested if statements
+
+
+    if ((hand1 == "rock" && hand2 == "rock") ||  (hand1 == "scissors" && hand2 == "scissors") || (hand1 == "paper" && hand2 == "paper")) {
+      return "It's a tie!"
+    }
+    else if ((hand1 == "rock" && hand2 == "scissors") || (hand1 == "scissors" && hand2 == "paper") || (hand1 == "paper" && hand2 == "rock")) {
+      return "Hand one wins!"
+    }
+    else if ((hand1 == "scissors" && hand2 == "rock") || (hand1 == "paper" && hand2 == "scissors") || (hand1 == "rock" && hand2 == "paper")) {
+      return "Hand two wins!"
+  
+  }
+  
+
+
+  }// end of function
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
@@ -30,12 +48,10 @@ function getPrompt() {
     });
   });
 }
-
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
 if (typeof describe === 'function') {
-
   // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
   describe('#rockPaperScissors()', () => {
     it('should detect a tie', () => {
@@ -55,8 +71,6 @@ if (typeof describe === 'function') {
     });
   });
 } else {
-
   // always returns ask the user for another input
   getPrompt();
-
 }
